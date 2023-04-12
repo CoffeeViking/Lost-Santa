918968a9437a5f5669a7d8bc06e69ab6e70323e0
CoffeeViking <91094682+CoffeeViking@users.noreply.github.com>
2023-04-12 17:58:49 -0400

    Update letters.py
    Added some comments for future reference.

--- letters.py
+++ letters.py
@@ -1,17 +1,35 @@
+# Uses pyfiglet to draw an input string, requires zero or two CLI arguments, requiring the first to be '-f' or '-font'
+# Zero arguments randomizes the string, or second argument defines the font to be used. 
+
 from pyfiglet import Figlet
 import random
-import sys
 figlet = Figlet()
+import sys
 fontList = figlet.getFonts()
+quote = ''
 
-if len(sys.argv) == 2 or len(sys.argv) >= 4:
-        print("Error: Error Code 123 easy as abc ")
+#Check for too many or too few arguments
+if len(sys.argv) >= 4:
+    print("Too many arguments, please try again!")
+    quit()
+elif len(sys.argv) == 2:
+        print("Can't use only one argument, Sorry!")
         quit()
+# Check that first argument is correct, then checks second against font list
+elif len(sys.argv) == 3:
+        if sys.argv[1] not in ('-f', '-font'):
+                print("Incorrect first argument, please try again!")
+                quit()
+        elif sys.argv[2] not in fontList:
+              print("Sorry, that isn't a valid font name!")
+              quit()
+        #Request string and print with defined text
+        else:
+            quote = input("Please enter a string!")
+            figlet.setFont(font=sys.argv[2])
+            print(figlet.renderText(quote))
+#Request string and print with random font        
 else:
-        quote = input("Please Enter a string of text!")
-if len(sys.argv) == 3:
-        figlet.setFont(font=sys.argv[2])
-        print(figlet.renderText(quote))
-else:
-        figlet.setFont(font=random.choice(fontList))
-        print(figlet.renderText(quote))
+    quote = input("Please enter a string!")
+    figlet.setFont(font=random.choice(fontList))
+    print(figlet.renderText(quote))
